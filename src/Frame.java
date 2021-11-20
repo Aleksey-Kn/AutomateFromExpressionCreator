5e1c11ec8967672e5ce05719c056e9629e56ce28
Aleksey-Kn <aleksey.k2000@mail.ru>
2021-11-20 16:09:37 +0700

    Ctart develop graphics

---
+++ src/Frame.java
@@ -0,0 +1,220 @@
+import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.io.*;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.TreeSet;
+import java.util.Vector;
+import java.util.stream.Collectors;
+
+public class Frame extends JFrame {
+    private JLabel canCreate = null;
+    private FromRegularExpressionGenerator fromRegularExpressionGenerator;
+
+    private Frame() {
+        super("Automate generator");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setBounds(200, 100, 650, 500);
+        setLayout(new BorderLayout());
+
+        JPanel inputPane = new JPanel();
+        inputPane.setLayout(new BoxLayout(inputPane, BoxLayout.Y_AXIS));
+        add(inputPane, BorderLayout.WEST);
+        JPanel chainsPane = new JPanel();
+        chainsPane.setLayout(new BoxLayout(chainsPane, BoxLayout.X_AXIS));
+        add(chainsPane, BorderLayout.CENTER);
+        JPanel automatePane = new JPanel();
+        automatePane.setLayout(new BoxLayout(automatePane, BoxLayout.Y_AXIS));
+        add(automatePane, BorderLayout.EAST);
+        JPanel sizePanel = new JPanel();
+        sizePanel.setLayout(new BoxLayout(sizePanel, BoxLayout.X_AXIS));
+        add(sizePanel, BorderLayout.SOUTH);
+        JPanel menuPanel = new JPanel();
+        menuPanel.setLayout(new BoxLayout(menuPanel, BoxLayout.X_AXIS));
+        add(menuPanel, BorderLayout.NORTH);
+
+        JPanel chains = new JPanel();
+        chains.setLayout(new BoxLayout(chains, BoxLayout.Y_AXIS));
+        chainsPane.add(chains);
+        JPanel checkChainsPane = new JPanel();
+        checkChainsPane.setLayout(new BoxLayout(checkChainsPane, BoxLayout.Y_AXIS));
+        chainsPane.add(checkChainsPane);
+
+        JButton about = new JButton("About author");
+        menuPanel.add(about);
+        JButton theme = new JButton("Theme");
+        menuPanel.add(theme);
+        JButton open = new JButton("Open initial data");
+        menuPanel.add(open);
+        JButton saveAutomate = new JButton("Save expression to file");
+        menuPanel.add(saveAutomate);
+        JButton saveChains = new JButton("Save chains to file");
+        menuPanel.add(saveChains);
+
+        sizePanel.add(new JLabel("Size of chains: "));
+        sizePanel.add(new JLabel("from "));
+        JTextField fromSize = new JTextField();
+        sizePanel.add(fromSize);
+        sizePanel.add(new JLabel(" to "));
+        JTextField toSize = new JTextField();
+        sizePanel.add(toSize);
+
+        inputPane.add(new JLabel("Regular expression:"));
+        JTextField expression = new JTextField();
+        inputPane.add(expression);
+        JButton chainsFromRegular = new JButton("Generate chains");
+        inputPane.add(chainsFromRegular);
+        JButton generateAutomate = new JButton("Generate automate");
+        inputPane.add(generateAutomate);
+
+        automatePane.add(new JLabel("Set of state:"));
+        JTextField states = new JTextField();
+        automatePane.add(states);
+        automatePane.add(new JLabel("Terminals symbol:"));
+        JTextField terminals = new JTextField();
+        automatePane.add(terminals);
+        automatePane.add(new JLabel("Start state:"));
+        JTextField startState = new JTextField();
+        automatePane.add(startState);
+        automatePane.add(new JLabel("End states:"));
+        JTextField endStates = new JTextField();
+        automatePane.add(endStates);
+        JButton update = new JButton("Update table");
+        automatePane.add(update);
+        DefaultTableModel tableModel = new DefaultTableModel() {
+            public boolean isCellEditable(int row, int column) {
+                return column != 0;
+            }
+        };
+        JTable regulations = new JTable(tableModel);
+        update.addActionListener(l -> {
+            Vector<String> columnIdentifier = new Vector<>(java.util.List.of(terminals.getText().split(" ")));
+            columnIdentifier.add(0, "");
+            tableModel.setColumnIdentifiers(columnIdentifier);
+            String[] rowsHandler = states.getText().split(" ");
+            int it = 0;
+            while (it < tableModel.getRowCount() && it < rowsHandler.length){
+                tableModel.setValueAt(rowsHandler[it], it, 0);
+                it++;
+            }
+            if(it < tableModel.getRowCount()) {
+                for(int i = tableModel.getRowCount() - it; i > 0; i--)
+                    tableModel.removeRow(tableModel.getRowCount() - 1);
+            } else {
+                while (it < rowsHandler.length) {
+                    tableModel.addRow(new String[]{rowsHandler[it++]});
+                }
+            }
+        });
+        automatePane.add(regulations);
+        JButton checkChains = new JButton("Check chains");
+        automatePane.add(checkChains);
+        JTextField userChain = new JTextField();
+        automatePane.add(userChain);
+        JButton checkUserChain = new JButton("Check this chain");
+        automatePane.add(checkUserChain);
+
+
+
+        chainsFromRegular.addActionListener(l -> {
+            chains.removeAll();
+            try {
+
+            } catch (Exception e) {
+                JLabel exceptionLabel = new JLabel(e.getMessage());
+                exceptionLabel.setForeground(Color.RED);
+                chains.add(exceptionLabel);
+                e.printStackTrace();
+            }
+            chainsPane.updateUI();
+        });
+
+        checkChains.addActionListener(l -> {
+            checkChainsPane.removeAll();
+            try {
+
+            } catch (Exception e) {
+                JLabel exceptionLabel = new JLabel(e.getMessage());
+                exceptionLabel.setForeground(Color.RED);
+                checkChainsPane.add(exceptionLabel);
+                e.printStackTrace();
+            }
+            chainsPane.updateUI();
+        });
+
+        about.addActionListener(l ->
+                JOptionPane.showMessageDialog(null, "Терентьев Данил, ИП-814"));
+        theme.addActionListener(l ->
+                JOptionPane.showMessageDialog(null,
+                        """
+                                Написать программу, которая по заданному регулярному выражению построит эквивалентный ДКА.
+                                Функция переходов ДКА может изображаться в виде таблицы и графа (выбор вида отображения посредством меню).
+                                Программа должна сгенерировать по РВ несколько цепочек в заданном диапазоне длин и проверить их допустимость построенным автоматом.
+                                Процесс разбора цепочек автоматом отображать на экране. Предусмотреть возможность разбора цепочки,
+                                введённой пользователем. В качестве исходных данных допускаются РВ, порождающие цепочки,
+                                имеющие определенное количество циклических повторений всех символов алфавита или некоторой их части, заканчивающиеся на заданную цепочку.
+                                """));
+        saveAutomate.addActionListener(l -> {
+            String name = JOptionPane.showInputDialog(null,
+                    "Введите имя файла для сохранения",
+                    "Сохранение автомата", JOptionPane.QUESTION_MESSAGE);
+            if(name != null) {
+                try {
+                    PrintWriter writer = new PrintWriter(new FileWriter(name));
+                    writer.print("M({");
+                    writer.print(states);
+                    writer.print("}, ");
+                    //writer.print();
+
+                    writer.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        saveChains.addActionListener(l -> {
+            String name = JOptionPane.showInputDialog(null,
+                    "Введите имя файла для сохранения",
+                    "Сохранение цепочек", JOptionPane.QUESTION_MESSAGE);
+            if (name != null) {
+                try {
+                    PrintWriter writer = new PrintWriter(new FileWriter(name + ".txt"));
+                    for (Component component : chainsPane.getComponents()) {
+                        writer.println(((Label) component).getText());
+                    }
+                    writer.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        open.addActionListener(l -> {
+            String name = JOptionPane.showInputDialog(null,
+                    "Введите имя файла",
+                    "Чтение регулярного выражения из файла", JOptionPane.QUESTION_MESSAGE);
+            if (name != null) {
+                try {
+                    Scanner scanner = new Scanner(new File(name + ".txt"));
+                    StringBuilder stringBuilder = new StringBuilder();
+                    while (scanner.hasNextLine())
+                        stringBuilder.append(scanner.nextLine());
+                    expression.setText(stringBuilder.toString());
+                    scanner.close();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Frame();
+    }
+}
---
+++ src/FromRegularExpressionGenerator.java
@@ -0,0 +1,122 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FromRegularExpressionGenerator {
+    private final String regular;
+    private int maxSize;
+
+    public FromRegularExpressionGenerator(String regular){
+        this.regular = regular.replace("\n", "");
+        if(regular.chars().filter(c -> c == '(').count() != regular.chars().filter(c -> c == ')').count())
+            throw new IllegalArgumentException("Count '(' not equal count ')'");
+        if(regular.chars().anyMatch(Character::isUpperCase))
+            throw new IllegalArgumentException("Regular expression must not contain uppercase symbol");
+    }
+
+    public List<String> generateChains(int minLength, int maxLength){
+        maxSize = maxLength;
+        ArrayList<String> results = new ArrayList<>();
+        results.add("");
+        if(regular.charAt(0) == '('
+                && regular.charAt(regular.length() - 1) == '*'
+                && indexCloseFor(regular) == regular.length() - 2)
+            results = manyStep(results, split(regular.substring(1, regular.length() - 2)));
+        else {
+            if(regular.charAt(0) == '(' && indexCloseFor(regular) == regular.length() - 1)
+                results = oneStep(results, split(regular.substring(1, regular.length() - 1)));
+            else
+                results = oneStep(results, split(regular));
+        }
+        return results.stream()
+                .filter(s -> s.length() <= maxLength)
+                .filter(s -> s.length() >= minLength)
+                .collect(Collectors.toList());
+    }
+
+    private ArrayList<String> oneStep(ArrayList<String> previousResult, String[] options){ // возвращает входные цепочки, на которые навешены варианты из входных options
+        ArrayList<String> tempResult;
+        ArrayList<String> newResult = new ArrayList<>();
+        StringBuilder state;
+        int ind;
+        for(String nowOptions: options){
+            tempResult = new ArrayList<>(previousResult);
+            state = new StringBuilder(nowOptions);
+            while (!state.isEmpty()){
+                if(state.charAt(0) == '('){
+                    ind = indexCloseFor(state.toString()); // индекс конца обрабатываемых скобок
+                    String now = state.substring(1, ind);
+                    if(ind != state.length() - 1 && state.charAt(ind + 1) == '*') {
+                        tempResult = manyStep(tempResult, split(now));
+                        state.delete(0, ind + 2);
+                    } else {
+                        tempResult = oneStep(tempResult, split(now));
+                        state.delete(0, ind + 1);
+                    }
+                } else{ // когда нет ветвления
+                    ind = state.indexOf("(");
+                    if(ind == -1)
+                        ind = state.length();
+                    String now = state.substring(0, ind);
+                    state.delete(0, ind);
+                    for(int i = 0; i < tempResult.size(); i++)
+                        tempResult.set(i, tempResult.get(i) + now);
+                }
+            }
+            newResult.addAll(tempResult);
+        }
+        return newResult;
+    }
+
+    private ArrayList<String> manyStep(ArrayList<String> previousResult, String[] options){
+        ArrayList<String> newResult = new ArrayList<>(previousResult);
+        ArrayList<String> tempResult = new ArrayList<>(previousResult);
+        while (true){
+            tempResult = oneStep(tempResult, options);
+            if(tempResult.stream().allMatch(e -> e.length() > maxSize)) {
+                return newResult.stream()
+                        .filter(e -> e.length() <= maxSize)
+                        .collect(Collectors.toCollection(ArrayList::new));
+            }
+            newResult.addAll(tempResult);
+        }
+    }
+
+    private int indexCloseFor(String nowRegular){
+        int count = 1;
+        char[] reg = nowRegular.toCharArray();
+        for(int i = 1; i < nowRegular.length(); i++){
+            if(reg[i] == '(')
+                count++;
+            else if(reg[i] == ')'){
+                count--;
+                if(count == 0)
+                    return i;
+            }
+        }
+        return -1;
+    }
+
+    private String[] split(String s){
+        LinkedList<String> spliting = new LinkedList<>();
+        int count = 0;
+        int prev = 0;
+        char[] reg = s.toCharArray();
+        for(int i = 0; i < s.length(); i++){
+            if(reg[i] == '+') {
+                if (count == 0) {
+                    spliting.add(s.substring(prev, i));
+                    prev = i + 1;
+                }
+            }
+            else if(reg[i] == '(')
+                count++;
+            else if(reg[i] == ')'){
+                count--;
+            }
+        }
+        spliting.add(s.substring(prev));
+        return spliting.toArray(String[]::new);
+    }
+}
