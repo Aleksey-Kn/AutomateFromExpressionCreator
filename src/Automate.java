04fe6aa71116f560ee45043be8923162ec3216c6
Aleksey-Kn <aleksey.k2000@mail.ru>
2021-11-20 19:59:55 +0700

    Рефакторинг автомата

--- src/Automate.java
+++ src/Automate.java
@@ -2,73 +2,54 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class Automate {
-    static class Rules{
-        private final char terminal;
-        private final String state;
-
-        Rules(String s){
-            s = s.trim();
-            terminal = s.charAt(0);
-            state = s.substring(1);
-        }
-    }
-
     private final Set<Character> terminal;
-    private final Map<String, Set<Rules>> regulations = new HashMap<>();
+    private final Map<String, Map<Character, String>> regulations = new HashMap<>();
     private final String startState;
     private final Set<String> endState;
 
-    public Automate(Set<Character> terminal, Set<String> state, Set<String> stringRules, String startState,
-                    Set<String> endState){
+    public Automate(Set<Character> terminal, Set<String> state, Set<String[]> rules, String startState,
+                    Set<String> endState) {
         this.terminal = terminal.stream().map(Character::toLowerCase).collect(Collectors.toSet());
         this.startState = startState;
         this.endState = endState;
 
-        String[] t;
-        String key;
-        Rules value;
-        for(String r: stringRules){
-            t = r.split("->");
-            key = t[0].trim();
-            value = new Rules(t[1]);
-            regulations.putIfAbsent(key, new HashSet<>());
-            regulations.get(key).add(value);
-            if(!state.contains(key))
-                throw new IllegalArgumentException("Use not exist non terminal symbol: " + key);
-            if(!this.terminal.contains(value.terminal) || !state.contains(value.state)) {
-                throw new IllegalArgumentException("Incorrect rules: " + r);
+        for (String[] r : rules) {
+            if (r[1].length() > 1)
+                throw new IllegalArgumentException("Terminal symbol in rules must be one symbol");
+            if (!state.contains(r[0]))
+                throw new IllegalArgumentException("Use not exist state: " + r[0]);
+            if (!this.terminal.contains(r[1].charAt(0)))
+                throw new IllegalArgumentException("Use not exist terminal: " + r[1]);
+            if (!state.contains(r[2])) {
+                throw new IllegalArgumentException("Use not exist state: " + r[2]);
             }
+            if (!regulations.containsKey(r[0]))
+                regulations.put(r[0], new HashMap<>());
+            regulations.get(r[0]).put(r[1].charAt(0), r[2]);
         }
     }
 
-    public boolean canCreate(String lang, StringBuilder cause, List<String> logs){
+    public boolean canCreate(String lang, StringBuilder cause, List<String> logs) {
         char[] language = lang.toCharArray();
         cause.delete(0, cause.length());
         logs.clear();
-        for(char c: language){
-            if(!terminal.contains(c)){
+        for (char c : language) {
+            if (!terminal.contains(c)) {
                 cause.append("Language exist unacceptable symbol");
                 return false;
             }
         }
         String nowState = startState;
-        boolean wasSearch;
-        for(int i = 0; i < language.length; i++){
-            wasSearch = false;
+        for (int i = 0; i < language.length; i++) {
             logs.add(nowState + ": " + lang.substring(i));
-            for(Rules nowVal: regulations.get(nowState)){
-                if(language[i] == nowVal.terminal){
-                    nowState = nowVal.state;
-                    wasSearch = true;
-                    break;
-                }
-            }
-            if(!wasSearch){
+            if (regulations.containsKey(nowState) && regulations.get(nowState).containsKey(language[i])) {
+                nowState = regulations.get(nowState).get(language[i]);
+            } else {
                 cause.append("The final state is unattainable");
                 return false;
             }
         }
-        if(endState.contains(nowState)) {
+        if (endState.contains(nowState)) {
             logs.add(nowState + ": ");
             return true;
         } else {
